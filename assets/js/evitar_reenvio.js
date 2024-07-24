6e7358257aba6bd60bf1a70960b5fd38bdc32d13
miguel <migueloned@gmail.com>
2024-07-24 16:26:34 -0400

    pagina_terminada

--- assets/js/evitar_reenvio.js
+++ assets/js/evitar_reenvio.js
@@ -1,3 +1,5 @@
+// codigo para evitar ventana emergente al recargar la pagina, por el envio del formulario
+
 if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href)
 }
